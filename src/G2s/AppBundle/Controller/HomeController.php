86bc2e6567c90f8f58cd02865b260d52c1a89447
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-04-02 21:28:40 +0200

    Test db query and display at url /apps

--- src/G2s/AppBundle/Controller/HomeController.php
+++ src/G2s/AppBundle/Controller/HomeController.php
@@ -10,4 +10,52 @@ class HomeController extends Controller
     {
         return $this->render('G2sAppBundle:Home:index.html.twig');
     }
+
+	public function appsAction()
+	{
+		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:Apps');
+		$queriedApps	= $repository->findAll();
+
+		$apps			= array();
+
+		foreach($queriedApps as $queriedApp)
+		{
+			$app				= array();
+			$globalAvgMark		= 0;
+
+			$app['app']			= $queriedApp;
+
+			$appInfo			= array();
+
+			foreach($queriedApp->getAppInfos() as $queriedAppInfo)
+			{
+				$avgMark	= 0;
+
+				foreach($queriedAppInfo->getMarks() as $mark)
+					$avgMark += $mark->getMark();
+				$avgMark /= count($queriedAppInfo->getMarks());
+
+				$appInfos[] = array(
+					'platform'	=> $queriedAppInfo->getPlatform(),
+					'comments'	=> $queriedAppInfo->getComments(),
+					'marks'		=> $queriedAppInfo->getMarks(),
+					'avgMark'	=> $avgMark
+				);
+
+				$globalAvgMark += $avgMark;
+			}
+
+			$app['appInfos']	= $appInfos;
+
+			$globalAvgMark /= count($appInfos);
+			$app['avgMark']		= $globalAvgMark;
+
+			$apps[]				= $app;
+		}
+
+		return $this->render(
+			'G2sAppBundle:Apps:list.html.twig',
+			array('apps' => $apps)
+		);
+	}
 }
