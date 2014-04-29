5678a2477127e20944b5cef2e2156787e35fc493
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-04-29 13:07:37 +0200

    Deleted app convertion and compute avg in template (optimisation, not exactly mvc though)

--- src/G2s/AppBundle/Controller/AppsController.php
+++ src/G2s/AppBundle/Controller/AppsController.php
@@ -17,87 +17,35 @@ class AppsController extends Controller
 	public function showOneAction($app_id)
 	{
 		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:App');
-		$queriedAppp;
+		$app;
 
 		if(is_numeric($app_id))
-			$queriedApp	= $repository->findOneById($app_id);
+			$app	= $repository->findOneById($app_id);
 		else if(is_string($app_id))
-			$queriedApp	= $repository->findOneByName($app_id);
+			$app	= $repository->findOneByName($app_id);
 
-		if($queriedApp == null)
+		if($app == null)
 			throw $this->createNotFoundException('App not found');
 
 		return $this->render(
 			'G2sAppBundle:Apps:show-one.html.twig',
-			array('app' => $this->convertApp($queriedApp))
+			array('app' => $app)
 		);
 	}
 
 	public function showAllAction()
 	{
 		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:App');
-		$queriedAppp;
 
-		$queriedApps	= $repository->findAll();
+		$apps			= $repository->findAll();
 
-		if($queriedApps == null)
+		if($apps == null)
 			throw $this->createNotFoundException('Apps not found');
-
-		$apps			= array();
-
-		$i = 0;
-		foreach($queriedApps as $queriedApp)
-		{
-			$apps[$i]	= $this->convertApp($queriedApp);
-			$i++;
-		}
 
 		return $this->render(
 			'G2sAppBundle:Apps:show-all.html.twig',
 			array('apps' => $apps)
 		);
-	}
-
-	public function convertApp($toConvertApp)
-	{
-		$app				= array();
-		$globalAvgMark		= 0;
-
-		$app['app']			= $toConvertApp;
-
-		$appInfos			= array();
-
-		$j = 0;
-
-		if(!is_null($toConvertApp->getAppInfos()))
-		{
-			foreach($toConvertApp->getAppInfos() as $queriedAppInfo)
-			{
-				$avgMark	= 0;
-
-				foreach($queriedAppInfo->getMarks() as $mark)
-					$avgMark += $mark->getMark();
-				if(count($queriedAppInfo->getMarks()) != 0)
-					$avgMark /= count($queriedAppInfo->getMarks());
-
-				$appInfos[$j] = array(
-					'platform'	=> $queriedAppInfo->getPlatform(),
-					'dlPath'	=> $queriedAppInfo->getDownloadPath(),
-					'comments'	=> $queriedAppInfo->getComments(),
-					'marks'		=> $queriedAppInfo->getMarks(),
-					'avgMark'	=> $avgMark
-				);
-
-				$globalAvgMark += $avgMark;
-			}
-		}
-
-		$app['appInfos']	= $appInfos;
-		if(count($appInfos))
-			$globalAvgMark /= count($appInfos);
-		$app['avgMark']		= $globalAvgMark;
-
-		return $app;
 	}
 }
 
