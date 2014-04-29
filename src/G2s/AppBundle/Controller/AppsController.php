619f0f09c73faec3a88e3357b90c32d303c675df
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-04-29 10:18:50 +0200

    Refactored app convertion and added apps/show to list all apps

--- src/G2s/AppBundle/Controller/AppsController.php
+++ src/G2s/AppBundle/Controller/AppsController.php
@@ -14,69 +14,90 @@ class AppsController extends Controller
         return $this->render('G2sAppBundle:Home:index.html.twig');
     }
 
-	public function showAction($app_id)
+	public function showOneAction($app_id)
 	{
 		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:App');
-		$queriedApps	= array();
+		$queriedAppp;
 
 		if(is_numeric($app_id))
-			$queriedApps[0]	= $repository->findOneById($app_id);
+			$queriedApp	= $repository->findOneById($app_id);
 		else if(is_string($app_id))
-			$queriedApps[0]	= $repository->findOneByName($app_id);
+			$queriedApp	= $repository->findOneByName($app_id);
 
-		if(sizeof($queriedApps) == 0 || $queriedApps[0] == null)
+		if($queriedApp == null)
 			throw $this->createNotFoundException('App not found');
+
+		return $this->render(
+			'G2sAppBundle:Apps:show-one.html.twig',
+			array('app' => $this->convertApp($queriedApp))
+		);
+	}
+
+	public function showAllAction()
+	{
+		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:App');
+		$queriedAppp;
+
+		$queriedApps	= $repository->findAll();
+
+		if($queriedApps == null)
+			throw $this->createNotFoundException('Apps not found');
 
 		$apps			= array();
 
 		$i = 0;
-
 		foreach($queriedApps as $queriedApp)
 		{
-			$app				= array();
-			$globalAvgMark		= 0;
-
-			$app['app']			= $queriedApp;
-
-			$appInfos			= array();
-
-			$j = 0;
-
-			if(!is_null($queriedApp->getAppInfos()))
-			{
-				foreach($queriedApp->getAppInfos() as $queriedAppInfo)
-				{
-					$avgMark	= 0;
-
-					foreach($queriedAppInfo->getMarks() as $mark)
-						$avgMark += $mark->getMark();
-					if(count($queriedAppInfo->getMarks()) != 0)
-						$avgMark /= count($queriedAppInfo->getMarks());
-
-					$appInfos[$j] = array(
-						'platform'	=> $queriedAppInfo->getPlatform(),
-						'dlPath'	=> $queriedAppInfo->getDownloadPath(),
-						'comments'	=> $queriedAppInfo->getComments(),
-						'marks'		=> $queriedAppInfo->getMarks(),
-						'avgMark'	=> $avgMark
-					);
-
-					$globalAvgMark += $avgMark;
-				}
-			}
-
-			$app['appInfos']	= $appInfos;
-			if(count($appInfos))
-				$globalAvgMark /= count($appInfos);
-			$app['avgMark']		= $globalAvgMark;
-
-			$apps[$i]				= $app;
+			$apps[$i]	= $this->convertApp($queriedApp);
 			$i++;
 		}
 
 		return $this->render(
-			'G2sAppBundle:Apps:show.html.twig',
+			'G2sAppBundle:Apps:show-all.html.twig',
 			array('apps' => $apps)
 		);
 	}
+
+	public function convertApp($toConvertApp)
+	{
+		$app				= array();
+		$globalAvgMark		= 0;
+
+		$app['app']			= $toConvertApp;
+
+		$appInfos			= array();
+
+		$j = 0;
+
+		if(!is_null($toConvertApp->getAppInfos()))
+		{
+			foreach($toConvertApp->getAppInfos() as $queriedAppInfo)
+			{
+				$avgMark	= 0;
+
+				foreach($queriedAppInfo->getMarks() as $mark)
+					$avgMark += $mark->getMark();
+				if(count($queriedAppInfo->getMarks()) != 0)
+					$avgMark /= count($queriedAppInfo->getMarks());
+
+				$appInfos[$j] = array(
+					'platform'	=> $queriedAppInfo->getPlatform(),
+					'dlPath'	=> $queriedAppInfo->getDownloadPath(),
+					'comments'	=> $queriedAppInfo->getComments(),
+					'marks'		=> $queriedAppInfo->getMarks(),
+					'avgMark'	=> $avgMark
+				);
+
+				$globalAvgMark += $avgMark;
+			}
+		}
+
+		$app['appInfos']	= $appInfos;
+		if(count($appInfos))
+			$globalAvgMark /= count($appInfos);
+		$app['avgMark']		= $globalAvgMark;
+
+		return $app;
+	}
 }
+
