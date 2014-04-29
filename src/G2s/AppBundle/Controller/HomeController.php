3598b0c4fb890457de51f4911c44be83cba6325b
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-04-29 11:33:33 +0200

    Added about and contact placeholder pages

--- src/G2s/AppBundle/Controller/HomeController.php
+++ src/G2s/AppBundle/Controller/HomeController.php
@@ -14,61 +14,14 @@ class HomeController extends Controller
         return $this->render('G2sAppBundle:Home:index.html.twig');
     }
 
-	public function appsAction()
+	public function aboutAction()
 	{
-		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:App');
-		$queriedApps	= $repository->findAll();
-
-		$apps			= array();
-
-		$i = 0;
-
-		foreach($queriedApps as $queriedApp)
-		{
-			$app				= array();
-			$globalAvgMark		= 0;
-
-			$app['app']			= $queriedApp;
-
-			$appInfos			= array();
-
-			$j = 0;
-
-			if(!is_null($queriedApp->getAppInfos()))
-			{
-				foreach($queriedApp->getAppInfos() as $queriedAppInfo)
-				{
-					$avgMark	= 0;
-
-					foreach($queriedAppInfo->getMarks() as $mark)
-						$avgMark += $mark->getMark();
-					if(count($queriedAppInfo->getMarks()) != 0)
-						$avgMark /= count($queriedAppInfo->getMarks());
-
-					$appInfos[$j] = array(
-						'platform'	=> $queriedAppInfo->getPlatform(),
-						'dlPath'	=> $queriedAppInfo->getDownloadPath(),
-						'comments'	=> $queriedAppInfo->getComments(),
-						'marks'		=> $queriedAppInfo->getMarks(),
-						'avgMark'	=> $avgMark
-					);
-
-					$globalAvgMark += $avgMark;
-				}
-			}
-
-			$app['appInfos']	= $appInfos;
-			if(count($appInfos))
-				$globalAvgMark /= count($appInfos);
-			$app['avgMark']		= $globalAvgMark;
-
-			$apps[$i]				= $app;
-			$i++;
-		}
-
-		return $this->render(
-			'G2sAppBundle:Apps:list.html.twig',
-			array('apps' => $apps)
-		);
+        return $this->render('G2sAppBundle:Home:about.html.twig');
 	}
+
+	public function contactAction()
+	{
+        return $this->render('G2sAppBundle:Home:contact.html.twig');
+	}
+
 }
