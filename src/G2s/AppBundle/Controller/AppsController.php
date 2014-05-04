53d26d72052d3110674d64fd7b37a543e0b590c6
MartinLG <martin.leguillou@y-nov.com>
2014-05-04 16:22:23 +0200

    commit fro search engine

--- src/G2s/AppBundle/Controller/AppsController.php
+++ src/G2s/AppBundle/Controller/AppsController.php
@@ -47,5 +47,94 @@ class AppsController extends Controller
 			array('apps' => $apps)
 		);
 	}
+
+	public function showSelectedAction()
+	{
+		$tagRepository		= $this->getDoctrine()->getRepository('G2sAppBundle:Tag');
+		$platformRepository = $this->getDoctrine()->getRepository('G2sAppBundle:Platform');
+		$tags 			= $tagRepository->findAll();
+		$platforms      = $platformRepository->findAll();
+		if($tags == null)
+			throw $this->createNotFoundException('Apps not found');
+
+		return $this->render(
+			'G2sAppBundle:Apps:show-selected.html.twig',
+			array('tags' => $tags, 'platforms' => $platforms)
+		);
+	}
+
+	public function selectAppsAction()
+	{
+	    $request = $this->container->get('request');
+
+	    
+	    	$em = $this->container->get('doctrine')->getEntityManager();
+
+	    	$search = "";
+	    	$search = $request->request->get('search');
+	        $mark = intval($request->request->get('mark'));
+	        $tags = json_decode($request->request->get('tagsQueried'), true);
+	        $platforms = json_decode($request->request->get('platformsQueried'), true);
+
+	        $tagsQuery = "";
+	        if ($tags['nbtags'] != 0) {
+	        	for ($i=0; $i < $tags['nbtags']; $i++) { 
+	        		if ($i == 0) {
+						$tagsQuery = $tagsQuery . " AND (tag.id = " . $tags['tag' . $i];
+					}else{
+						$tagsQuery = $tagsQuery . " OR tag.id = " . $tags['tag' . $i];
+					}
+	        	}
+	        	
+	        	$tagsQuery = $tagsQuery . ")";
+	        }
+
+	        $platformsQuery = "";
+	        if ($platforms['nbplatforms'] != 0) {
+	        	for ($i=0; $i < $platforms['nbplatforms']; $i++) { 
+	        		if ($i == 0) {
+						$platformsQuery = $platformsQuery . " AND (appinfos.platform = " . $platforms['platform' . $i];
+					}else{
+						$platformsQuery = $platformsQuery . " OR appinfos.platform = " . $platforms['platform' . $i];
+					}
+	        	}
+	        	
+	        	$platformsQuery = $platformsQuery . ")";
+	        }
+
+	        $searchQuery = "";
+	        if ($search != ""){
+	        	$searchQuery = " AND (app.name LIKE '%" . $search . "%')";
+	        }
+	        
+	        $secndquery = 'SELECT app FROM G2sAppBundle:App app
+				              JOIN app.appInfos appinfos
+				              JOIN app.tags tag
+				              WHERE appinfos.average_mark >= :mark' . $tagsQuery . $platformsQuery . $searchQuery;
+        
+            $query = $em
+				          ->createQuery(
+				              $secndquery
+				          )->setParameter('mark', $mark);
+
+            $apps = $query->getResult();
+
+
+	        
+	    if($request->isXmlHttpRequest())
+	    {
+	    	return $this->container->get('templating')->renderResponse('G2sAppBundle:Apps:show-all-nolayout.html.twig', array(
+	            'apps' => $apps,
+	            ));
+	    }else{
+	    	
+			return $this->container->get('templating')->renderResponse('G2sAppBundle:Apps:show-all.html.twig', array(
+	            'apps' => $apps,
+
+	            ));
+		
+	    }
+		
+	}
 }
 
