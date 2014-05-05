226a3a3e876d84086e1ba67a30b9b6a2be3534c9
MartinLG <martin.leguillou@y-nov.com>
2014-05-05 15:01:49 +0200

    Comments done, but still need return for user.

--- src/G2s/AppBundle/Controller/AppsController.php
+++ src/G2s/AppBundle/Controller/AppsController.php
@@ -68,56 +68,56 @@ class AppsController extends Controller
 	    $request = $this->container->get('request');
 
 	    
-	    	$em = $this->container->get('doctrine')->getEntityManager();
+	    $em = $this->container->get('doctrine')->getEntityManager();
 
-	    	$search = "";
-	    	$search = $request->request->get('search');
-	        $mark = intval($request->request->get('mark'));
-	        $tags = json_decode($request->request->get('tagsQueried'), true);
-	        $platforms = json_decode($request->request->get('platformsQueried'), true);
+	    $search = "";
+	    $search = $request->request->get('search');
+	    $mark = intval($request->request->get('mark'));
+	    $tags = json_decode($request->request->get('tagsQueried'), true);
+	    $platforms = json_decode($request->request->get('platformsQueried'), true);
 
-	        $tagsQuery = "";
-	        if ($tags['nbtags'] != 0) {
-	        	for ($i=0; $i < $tags['nbtags']; $i++) { 
-	        		if ($i == 0) {
-						$tagsQuery = $tagsQuery . " AND (tag.id = " . $tags['tag' . $i];
-					}else{
-						$tagsQuery = $tagsQuery . " OR tag.id = " . $tags['tag' . $i];
-					}
-	        	}
+        $tagsQuery = "";
+        if ($tags['nbtags'] != 0) {
+	       	for ($i=0; $i < $tags['nbtags']; $i++) { 
+	       		if ($i == 0) {
+					$tagsQuery = $tagsQuery . " AND (tag.id = " . $tags['tag' . $i];
+				}else{
+					$tagsQuery = $tagsQuery . " OR tag.id = " . $tags['tag' . $i];
+				}
+	       	}
 	        	
-	        	$tagsQuery = $tagsQuery . ")";
-	        }
+	       	$tagsQuery = $tagsQuery . ")";
+	    }
 
-	        $platformsQuery = "";
-	        if ($platforms['nbplatforms'] != 0) {
-	        	for ($i=0; $i < $platforms['nbplatforms']; $i++) { 
-	        		if ($i == 0) {
-						$platformsQuery = $platformsQuery . " AND (appinfos.platform = " . $platforms['platform' . $i];
-					}else{
-						$platformsQuery = $platformsQuery . " OR appinfos.platform = " . $platforms['platform' . $i];
-					}
-	        	}
-	        	
-	        	$platformsQuery = $platformsQuery . ")";
-	        }
+	    $platformsQuery = "";
+	    if ($platforms['nbplatforms'] != 0) {
+	      	for ($i=0; $i < $platforms['nbplatforms']; $i++) { 
+	       		if ($i == 0) {
+					$platformsQuery = $platformsQuery . " AND (appinfos.platform = " . $platforms['platform' . $i];
+				}else{
+					$platformsQuery = $platformsQuery . " OR appinfos.platform = " . $platforms['platform' . $i];
+				}
+	       	}
+	       	
+	       	$platformsQuery = $platformsQuery . ")";
+	    }
 
-	        $searchQuery = "";
-	        if ($search != ""){
-	        	$searchQuery = " AND (app.name LIKE '%" . $search . "%')";
-	        }
+	    $searchQuery = "";
+	    if ($search != ""){
+	       	$searchQuery = " AND (app.name LIKE '%" . $search . "%')";
+	    }
 	        
-	        $secndquery = 'SELECT app FROM G2sAppBundle:App app
-				              JOIN app.appInfos appinfos
-				              JOIN app.tags tag
-				              WHERE appinfos.average_mark >= :mark' . $tagsQuery . $platformsQuery . $searchQuery;
+	    $secndquery = 'SELECT app FROM G2sAppBundle:App app
+			              JOIN app.appInfos appinfos
+			              JOIN app.tags tag
+			              WHERE appinfos.average_mark >= :mark' . $tagsQuery . $platformsQuery . $searchQuery;
         
-            $query = $em
-				          ->createQuery(
-				              $secndquery
-				          )->setParameter('mark', $mark);
+        $query = $em
+		          ->createQuery(
+		              $secndquery
+		          )->setParameter('mark', $mark);
 
-            $apps = $query->getResult();
+        $apps = $query->getResult();
 
 
 	        
--- src/G2s/AppBundle/Controller/CommentsController.php
+++ src/G2s/AppBundle/Controller/CommentsController.php
@@ -4,15 +4,47 @@ namespace G2s\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use G2s\AppBundle\Entity\App;
+use G2s\AppBundle\Entity\Mark;
+use G2s\AppBundle\Entity\Comment;
 use G2s\AppBundle\Entity\Platform;
 use G2s\AppBundle\Entity\AppInfo;
 
 class CommentsController extends Controller
 {
-	public function addAction($app_id)
+	public function addAction()
 	{
-		// TODO
-		return 'TODO';
+		$request = $this->container->get('request');
+
+	    $em = $this->container->get('doctrine')->getEntityManager();
+		
+		$mark = $request->request->get('mark');
+		$title = $request->request->get('title');
+		$commentText = $request->request->get('comment');
+		$platform = $request->request->get('platform');
+		$app = $request->request->get('app');
+
+		$comment = new Comment();
+
+		$appInfo = $em->getRepository('G2sAppBundle:AppInfo')->findBy(array('app' => intval($app), 'platform' => intval($platform)));
+
+		$comment->setAppInfo($appInfo[0]);
+		$comment->setTitle($title);
+		$comment->setComment($commentText);
+
+		if(isset($mark)){
+			$newMark = new Mark();
+			$newMark->setAppInfo($appInfo[0]);
+			$newMark->setMark($mark);
+
+			$em->persist($newMark);
+			$em->flush();
+
+			$comment->setMark($newMark);
+		}
+
+		$em->persist($comment);
+		$em->flush();
+		return new JsonResponse(array('message' => "Your comment was added successfully !"));
 	}
 }
 
