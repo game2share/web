5c728cad6da48341027d56de1de8fdb316c56542
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-05-06 18:45:02 +0200

    Tag page

--- src/G2s/AppBundle/Controller/TagsController.php
+++ src/G2s/AppBundle/Controller/TagsController.php
@@ -11,8 +11,21 @@ class TagsController extends Controller
 {
 	public function showAction($tag_id)
 	{
-		// TODO
-		return 'TODO';
+		$repository		= $this->getDoctrine()->getRepository('G2sAppBundle:Tag');
+		$tag			= null;
+
+		if(is_numeric($tag_id))
+			$tag	= $repository->findOneById($tag_id);
+		else if(is_string($tag_id))
+			$tag	= $repository->findOneByName($tag_id);
+
+		if($tag == null)
+			throw $this->createNotFoundException('Tag not found');
+
+		return $this->container->get('templating')->renderResponse('G2sAppBundle:Tags:show-all.html.twig', array(
+			'tagName' => $tag->getName(),
+			'apps' => $tag->getApps()
+		));
 	}
 }
 
